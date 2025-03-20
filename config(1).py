a896c66a097c6d327392455795baa7ecc7fd3a81
LANDLELEMERA <rolexowner12@gmail.com>
2025-03-20 14:26:39 +0530

    Add files via upload

---
+++ config(1).py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY@RARExOWNER
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7992702114:AAFt9eu4bzeS7MjeANbFLr03S9uRsLLPL4c")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("RARExOWNER")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("7329403339")
+
+
+
+
+
+
+
+  
